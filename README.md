a1d84a1ae8ef1fb1c8f72440ee7453db6781b27b
kely <kelly_le_23mai@hotmail.com>
2020-10-07 21:01:57 -0300

    API simulada  com dados estaticos

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# api-node-express
+# Express + NodeJS
+Nosso exemplo de API utilizando com o Express
+
+## Instalção do Express
+
+```sh
+$ npm install --save express
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const dados = require('./db.json');
+
+app.use(bodyParser.json());
+
+app.get('/livros', (req, res) => {
+    res.send(dados.livros);
+});
+
+app.get('/livros/:id', (req, res) => {
+    const livro = dados.livros.filter((elemento) =>{
+        if(elemento.id == req.params.id){
+            return true;
+        }else {
+            return false;
+        }
+    });
+    res.send(...livro);
+});
+
+app.post('/livros' ,(req, res) =>{
+    res.send('Livros cadastrado com sucesso - ' + req.body.titulo);
+});
+
+app.put('/livros/:id', (req, res) => {
+    res.send('Livro atualizado com sucesso');
+});
+
+app.delete('/livros/:id', (req, res) => {
+    res.send('Livro apagado com sucesso');
+});
+
+app.listen(3000);
